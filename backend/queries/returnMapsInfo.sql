4e47aa5496901a3733822c5cb0d2946a374fd9d2
joehtet <joehtet@gmail.com>
2019-11-27 01:51:54 -0500

    add missing queries folder

---
+++ backend/queries/returnMapsInfo.sql
@@ -0,0 +1,33 @@
+SELECT map_id, semester_num, F.course_id, G.subject, G.catalog, descr, min_units, max_units, designation
+FROM (
+SELECT map_id, semester_num, course1 AS 'course_id'
+FROM MAPS_DMAPS_LISTS
+WHERE map_id = @map_id AND !ISNULL(course1)
+UNION
+select map_id, semester_num, course2 AS 'course_id'
+FROM MAPS_DMAPS_LISTS
+WHERE map_id = @map_id AND !ISNULL(course2)
+UNION
+select map_id, semester_num, course3 AS 'course_id'
+FROM MAPS_DMAPS_LISTS
+WHERE map_id = @map_id AND !ISNULL(course3)
+UNION
+select map_id, semester_num, course4 AS 'course_id'
+FROM MAPS_DMAPS_LISTS
+WHERE map_id = @map_id AND !ISNULL(course4)
+UNION
+select map_id, semester_num, course5 AS 'course_id'
+FROM MAPS_DMAPS_LISTS
+WHERE map_id = @map_id AND !ISNULL(course5)
+UNION
+select map_id, semester_num, course6 AS 'course_id'
+FROM MAPS_DMAPS_LISTS
+WHERE map_id = @map_id AND !ISNULL(course6)
+UNION
+select map_id, semester_num, course7 AS 'course_id'
+FROM MAPS_DMAPS_LISTS
+WHERE map_id = @map_id AND !ISNULL(course7)
+) AS F
+INNER JOIN MAPS_CRSE_CATALOG AS G
+	ON F.course_id = G.course_id
+ORDER BY map_id, semester_num, course_id
